8256692c145c4c16818407873dee52bcfb288629
nikhilrajaram <nira8877@colorado.edu>
2020-07-08 20:08:09 -0600

    refactor base class name in example

--- example.py
+++ example.py
@@ -4,6 +4,6 @@ from UnimplementedType import UnimplementedType
 if __name__ == '__main__':
     # using https://jsonplaceholder.typicode.com/users for an example payload
     response = requests.get(url='https://jsonplaceholder.typicode.com/users')
-    print(UnimplementedType('payload')
+    print(UnimplementedType('user')
           .from_json(response.json())
           .codegen(include_nested_classes=True))
