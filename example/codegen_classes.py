b718dd07bffb29c0e0d476f5530f4a40698c3018
nikhilrajaram <nira8877@colorado.edu>
2020-07-15 12:11:40 -0600

    use different example api

--- example/codegen_classes.py
+++ example/codegen_classes.py
@@ -2,10 +2,10 @@ import requests
 from UnimplementedType import UnimplementedType
 
 if __name__ == '__main__':
-    # using https://jsonplaceholder.typicode.com/users for an example payload
-    response = requests.get(url='https://jsonplaceholder.typicode.com/users')
-    out = UnimplementedType('user') \
-        .unimplementedtype_from_json(response.json()) \
+    # using https://pokeapi.co/api/v2/pokemon/ditto for an example payload
+    response = requests.get(url='https://pokeapi.co/api/v2/pokemon/ditto')
+    out = UnimplementedType('ditto')\
+        .serialize_json(response.json())\
         .codegen(include_nested_classes=True, include_from_json_method=True)
 
     with open('example/out.py', 'w') as f:
